352a36c063c6b3fb7e04008b34ca1fe1e0463e58
TaeYoon17 <kim0517@hotmail.com>
2023-08-08 14:49:24 +0900

    Initial Commit

---
+++ assign/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assign
+//
+//  Created by 김태윤 on 2023/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
